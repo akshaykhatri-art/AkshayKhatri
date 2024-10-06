9a4df7e9d265d3af7950056f0d8481e15f15da0a
Akshay <akshaykhatri22@gmail.com>
2024-10-07 02:37:37 +0530

    Meta data changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,13 +6,19 @@ const Footer = () => {
     <div className="px-6 md:px-0 mt-12 text-white/70 py-8 max-w-[1000px] container mx-auto border-t border-gray-700 pt-4 flex justify-between items-center">
       <h1 className="text-2xl font-bold">Akshay Khatri</h1>
       <div className="flex space-x-6 mt-4">
-        <a href="#" className="hover:text-gray-300">
+        <a
+          href="https://www.linkedin.com/in/akshay-khatri-923230146"
+          className="hover:text-gray-300"
+        >
           <FaLinkedin size={24} />
         </a>
-        <a href="#" className="hover:text-gray-300">
+        <a href="https://x.com/born__artist" className="hover:text-gray-300">
           <FaTwitter size={24} />
         </a>
-        <a href="#" className="hover:text-gray-300">
+        <a
+          href="https://www.instagram.com/akshaykhatri_art/"
+          className="hover:text-gray-300"
+        >
           <FaInstagram size={24} />
         </a>
       </div>
